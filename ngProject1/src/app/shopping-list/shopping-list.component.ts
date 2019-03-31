35a8e91fb56e8a35e96dcb8570e79d80368eba5d
angelaV <bing.victorio@aa.com>
2019-03-30 23:05:26 -0500

    fixed ingredients

--- ngProject1/src/app/shopping-list/shopping-list.component.ts
+++ ngProject1/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
@@ -7,7 +8,7 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingreidents: Ingredient[] = [
+  ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
   ];
